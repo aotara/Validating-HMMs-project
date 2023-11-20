7ae388da7da9a7e94cfb69cb7166eb51c1afcef8
aotara <56065221+aotara@users.noreply.github.com>
2023-11-20 18:56:34 +0000

    Add files via upload

---
+++ figure1.R
@@ -0,0 +1,61 @@
+################################################################################
+# Load Libraries 
+library(ggplot2)
+library(tidyverse)
+library(marmap)
+library(ggsn)
+library(ggspatial)
+library(dplyr)
+library(ggrepel)
+library(sf)
+
+################################################################################
+# Figure 1 Page 5 - Map for study sites
+################################################################################
+
+# creating dataframe longitude,latitude of colonies
+df1 <- data.frame(lat = c(54.8197, 53.41001, 54.6, 55.38, 57.307165438,
+                          56.49629974, 56.3, 56, 56.20), 
+                  lng = c(-5.7, -4.51393, -5.5, -1.53850207, -1.98499606,
+                          -5.71587596, -2.566667, -3.167832662, -5.6),
+                  colony = c('Blue Circle', 'Cemlyn Bay', 'Cockle Island',
+                             'Coquet Island', 'Forvie', 'Glas Eileanan',
+                             'Isle of May', 'Leith', 'South Shian'),
+                  num=c(1,2,3,4,5,6,7,8,9), stringsAsFactors = FALSE)
+# legend
+df2 <- data.frame(lat = c(59.2,58.8,58.3,57.8,57.3,56.8,56.3,55.8,55.3,54.8), 
+                  lng = c(0.8,0.63,0.7,0.8,1.1,0.3,0.8,0.6,0.2,0.68),
+                  colony = c('Study site (tern species)','1. Blue Circle (R,S)',
+                             '2. Cemlyn Bay (A,C)','3. Cockle Island (A,S)',
+                             '4. Coquet Island (A,C,R,S)','5. Forvie (S)',
+                             '6. Glas Eileanan (C)','7. Isle of May (A)',
+                             '8. Leith (C)','9. South Shian (C)'),
+                  stringsAsFactors = FALSE)
+# get bathymetry
+colony <- getNOAA.bathy(lon1 = -12, lon2 = 3,lat1 = 49.5, lat2 = 59.5, 
+                        resolution = 5)
+# set breaks and colours
+breaks <- seq(-2000, 0, by = 2000)
+cols   <- c('white', 'white', 'white', 'grey60', 'grey60')
+
+autoplot(colony, geom = c('r')) +
+  scale_fill_stepsn(colours = cols, breaks = breaks, guide = NULL)
+# plot
+last_plot()+
+  geom_text(data = df1, aes(x = lng, y = lat, label = num), size = 7,
+            colour = 'blue2',fontface = "bold")+
+  geom_text(data = df2,aes(x = lng, y = lat, label = colony), size = 5,
+            colour = 'darkblue',fontface = "bold")+
+  # axis labels and plot theme
+  xlab(expression(paste("Longitude (\u00B0 W)"))) +
+  ylab(expression(paste("Latitude (\u00B0 N)"))) +
+  theme(axis.text = element_text(size = 20, colour = "black", face = "bold"),
+        axis.title = element_text(size = 25,colour = 'black', face = "bold"),
+        panel.border = element_rect(colour = "black", fill = NA, size = 2))+
+  # scalebar 
+  scalebar(x.min = -11, x.max = -7, y.min = 50, y.max = 50.3, st.bottom = T,
+           dist = 50, dist_unit = "km", transform = T, model = "WGS84", 
+           anchor = c(x = -10, y = 50.3), height = 0.7, st.size = 5, st.dist = 1,
+           box.fill = c("black", "white", "black", "white"))+
+                                       coord_cartesian(expand = 0)                                                                                                                                                                                              
+  
---
+++ figure2.R
@@ -0,0 +1,90 @@
+
+
+################################################################################
+#    Figure 2 Page 7 -  Map for Leith colony
+################################################################################
+
+# Load Libraries 
+library(ggplot2)
+library(tidyverse)
+library(marmap)
+library(ggsn)
+library(dplyr)
+
+## SET WORKING DIRECTORY
+setwd(" ")
+
+# read data 
+Dcr<-read.csv('Fig2/Leith_CR_2010.csv') # chick rearing
+Din<-read.csv('Fig2/Leith_IN_2010.csv') # incubation
+
+# replacing 'travelling' with ,not-foraging'
+Dcr<-Dcr%>%
+  mutate(observed_state=replace(observed_state,observed_state=='Travelling',
+                                'Not-foraging'))
+Din<-Din%>%
+  mutate(observed_state=replace(observed_state,observed_state=='Travelling',
+                                'Not-foraging'))
+# function to plot map
+leithPlot<- function(data,dd,dc){
+  # get bathymetry
+  leith <- getNOAA.bathy(lon1 = -3.33, lon2 = -2.95, lat1 = 55.945, lat2 =56.155, 
+                         resolution = 0.1)
+  # set breaks and colours
+  breaks <- seq(0, 300, by = 150)
+  cols <- c('white','white','grey60','grey60','grey60','grey60','grey60')
+  
+  autoplot(leith, geom = c('r')) + 
+    scale_fill_stepsn(colours = cols, guide = NULL, breaks =breaks)
+  # plot
+  k <- last_plot()+
+    geom_path(data = data, aes(x = LONGITUDE, y = LATITUDE, group = ID,
+                               color = `observed_state`), size = 0.7) +
+    scale_colour_manual(values = c(Foraging = 'darkorange2',
+                                   `Not-foraging` = 'steelblue1'))+
+    # site location
+    geom_point(aes(x = -3.179, y = 55.99), shape = '\u2605', colour = 'black',
+               size = 6) +
+    geom_text(aes(x = -3.18, y = 55.975, label = "Leith"), size = 8,
+              colour = 'black')+
+    # legend
+    geom_text(aes(x = -3.22, y = 56.185, label = "Observed State"), size = 8,
+              colour = 'black')+
+    geom_path(data = dd, aes(x = lng, y = lat), size = 1, colour = 'darkorange2')+
+    geom_text(aes(x = -3.25, y = 56.165, label = "Foraging"), size = 7, 
+              colour = 'black')+
+    geom_path(data = dc, aes(x = lng, y = lat), size = 1,colour = 'steelblue1')+
+    geom_text(aes(x = -3.235, y = 56.145, label = "Not-foraging"), size = 7, 
+              colour = 'black')+
+    # axis labels and plot theme
+    xlab("Longitude (\u00B0 W)") + ylab("Latitude (\u00B0 N)") +
+    theme(axis.text = element_text(size = 22, colour = "black", face = "bold"),
+          axis.title = element_text(size = 25, colour = "black", face = "bold"),
+          panel.border = element_rect(colour = "black", fill = NA, size = 2),
+          legend.position = 'none')+
+    # scalebar and north arrow
+    scalebar(x.min = -3.33, x.max = -2.95, y.min = 55.945, y.max = 56.155, 
+             st.bottom = T, dist = 5, dist_unit = "km", transform = T,
+             model = "WGS84", anchor = c(x = -3.25, y = 55.92), height = 0.03, 
+             st.size = 5, st.dist = 0.05, box.fill = c("black", "white",
+                                                        "black", "white"))+
+    ggspatial::annotation_north_arrow(location = "tl", which_north = "true",
+                           pad_x = unit(0.1, "in"), pad_y = unit(0.2, "in"),
+          style = ggspatial::north_arrow_nautical(fill = c("grey40", "white"), 
+                      line_col = "grey20", text_family = "ArcherPro Book"))+ 
+    coord_cartesian(expand = 0)
+  return(k)
+}
+# plot figure 2a
+# set data points for legend - incubation
+dd <- data.frame(lat = c(56.165, 56.165), lng = c(-3.36, -3.33)) # incubation
+dc <- data.frame(lat = c(56.15, 56.15), lng = c(-3.36, -3.33)) # incubation
+# plot 
+leithPlot(Din,dd,dc) 
+
+# plot figure 2b
+# set data points for legend - chick-rearing
+dd <- data.frame(lat = c(56.165, 56.165), lng = c(-3.33, -3.30)) # chick-rearing
+dc <- data.frame(lat = c(56.145, 56.145), lng = c(-3.33, -3.30)) # chick-rearing
+# plot 
+leithPlot(Dcr,dd,dc)
